c970ce43cb8b89f84f6c87b5b2f2bbf463ba6f7f
Mustafa Çiçek <f191220093@ktun.edu.tr>
2022-08-24 21:17:19 +0300

    Initial Commit

---
+++ CryptoAppSwiftUI/CryptoAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppSwiftUIApp.swift
+//  CryptoAppSwiftUI
+//
+//  Created by Mustafa Çiçek on 24.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

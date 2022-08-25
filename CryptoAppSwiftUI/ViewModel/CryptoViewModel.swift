e48214b5d07195a8cb0b96f64df5497fe047ac2a
Mustafa Çiçek <f191220093@ktun.edu.tr>
2022-08-26 00:07:44 +0300

    Processing the data from the service and sending it to the UI is completed.

--- CryptoAppSwiftUI/ViewModel/CryptoViewModel.swift
+++ CryptoAppSwiftUI/ViewModel/CryptoViewModel.swift
@@ -7,12 +7,26 @@
 import Foundation
 
 class CryptoListViewModel: ObservableObject {
+    
+    // published edip baska bir yerden subscrib olup dinleyebiliriz
+    @Published var cryptoList = [CryptoViewModel]()
+    
     let cryptoService = CryptoService()
     
     func downloadCrypto(url: URL){
         cryptoService.downloadCurrencies(url: url) { result in
             switch(result){
-                case 
+            case .failure(let error):
+                print(error)
+            case .success(let cryptos):
+                if let cryptos = cryptos {
+                    // pbulished after that UI changed and than we will need that main thread
+                    DispatchQueue.main.async {
+                        self.cryptoList = cryptos.map(CryptoViewModel.init)
+
+                    }
+                }
+   
             }
         }
     }
